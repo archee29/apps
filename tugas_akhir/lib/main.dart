7528ecf1d153ebbda7a55d6bb750398831b36088
archee29 <mrarchee01@gmail.com>
2023-08-20 02:01:57 +0700

    sync firebase di apps

--- tugas_akhir/lib/main.dart
+++ tugas_akhir/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tugas_akhir/auth_controller.dart';
 import 'package:tugas_akhir/login.dart';
-import 'package:tugas_akhir/signup_page.dart';
 import 'package:tugas_akhir/home.dart';
+// import 'package:firebase_app/signup_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value)=> Get.put(AuthController()));
   runApp(const CatApp());
 }
 
@@ -12,7 +17,7 @@ class CatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Automatic Cat Feeder',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
