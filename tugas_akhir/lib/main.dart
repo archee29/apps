c24cb6c32b42e38ac8c12fc60700d1144f660b1b
archee29 <mrarchee01@gmail.com>
2023-08-21 00:41:16 +0700

    config front-end

--- tugas_akhir/lib/main.dart
+++ tugas_akhir/lib/main.dart
@@ -1,28 +1,61 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:tugas_akhir/controllers/auth_controller.dart';
+// import 'package:tugas_akhir/login.dart';
+// import 'package:tugas_akhir/splash_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+//   runApp(const CatApp());
+// }
+
+// class CatApp extends StatelessWidget {
+//   const CatApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'Automatic Cat Feeder',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginPage(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tugas_akhir/auth_controller.dart';
-import 'package:tugas_akhir/login.dart';
-import 'package:tugas_akhir/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tugas_akhir/controllers/auth_controller.dart';
+import 'package:tugas_akhir/controllers/auth_controller.dart';
+import 'package:tugas_akhir/screens/signup_page.dart';
+import 'package:tugas_akhir/utils/constants.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
-  runApp(const CatApp());
+  await firebaseInitialization.then((value) => {
+        Get.put(AuthController()),
+      });
+  runApp(const MyApp());
 }
 
-class CatApp extends StatelessWidget {
-  const CatApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Automatic Cat Feeder',
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
+      home: const SignUp(),
     );
   }
 }
