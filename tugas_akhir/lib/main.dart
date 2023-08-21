f3afba5af89c98fffdaa7adea165d4525e740adb
archee29 <mrarchee01@gmail.com>
2023-08-22 02:14:50 +0700

    fixing comment yang tidak digunakan

--- tugas_akhir/lib/main.dart
+++ tugas_akhir/lib/main.dart
@@ -28,36 +28,3 @@ class CatApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:tugas_akhir/controllers/auth_controller.dart';
-// import 'package:tugas_akhir/controllers/auth_controller.dart';
-// import 'package:tugas_akhir/screens/signup_page.dart';
-// import 'package:tugas_akhir/utils/constants.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await firebaseInitialization.then((value) => {
-//         Get.put(AuthController()),
-//       });
-//   runApp(const CatApp());
-// }
-
-// class CatApp extends StatelessWidget {
-//   const CatApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Tugas Akhir',
-//       theme: ThemeData(
-//         primarySwatch: Colors.pink,
-//       ),
-//       home: const SignUp(),
-//     );
-//   }
-// }
