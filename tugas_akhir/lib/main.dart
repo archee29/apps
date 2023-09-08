730e41ed17f6204922793ea2ed32b532f7882844
archee29 <mrarchee01@gmail.com>
2023-09-09 06:24:47 +0700

    edit main class file

--- tugas_akhir/lib/main.dart
+++ tugas_akhir/lib/main.dart
@@ -5,40 +5,72 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tugas_akhir/app/controllers/auth_controller.dart';
 import 'package:tugas_akhir/app/routes/app_pages.dart';
 import 'package:tugas_akhir/app/utils/loading.dart';
-import 'package:get_storage/get_storage.dart';
+import 'package:tugas_akhir/app/controllers/page_index_controller.dart';
+import 'package:tugas_akhir/app/controllers/feeder_controller.dart';
+import 'firebase_options.dart';
+import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(CatApp());
-}
-
-class CatApp extends StatelessWidget {
-  // const CatApp({super.key});
-  final authC = Get.put(AuthController(), permanent: true);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: authC.streamAuthStatus,
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Get.put(FeederController(), permanent: true);
+  Get.put(PageIndexController(), permanent: true);
+  runApp(
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          print(snapshot.data);
-          return GetMaterialApp(
-            title: 'Automatic Cat Feeder',
-            debugShowCheckedModeBanner: false,
-
-            // initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
-            initialRoute:
-                snapshot.data != null && snapshot.data!.emailVerified == true
-                    ? Routes.HOME
-                    : Routes.LOGIN,
-            getPages: AppPages.routes,
-            // home: snapshot.data != null ? HomeView() : LoginView(),
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           );
         }
-        return LoadingView();
+        return GetMaterialApp(
+          title: "Automatic Cat Feeder",
+          debugShowCheckedModeBanner: false,
+          initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+          getPages: AppPages.routes,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            fontFamily: 'inter',
+          ),
+        );
       },
-    );
-  }
+    ),
+  );
 }
+
+// class CatApp extends StatelessWidget {
+//   // const CatApp({super.key});
+//   final authC = Get.put(AuthController(), permanent: true);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder<User?>(
+//       stream: authC.streamAuthStatus,
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.active) {
+//           print(snapshot.data);
+//           return GetMaterialApp(
+//             title: 'Automatic Cat Feeder',
+//             debugShowCheckedModeBanner: false,
+
+//             // initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+//             initialRoute:
+//                 snapshot.data != null && snapshot.data!.emailVerified == true
+//                     ? Routes.HOME
+//                     : Routes.LOGIN,
+//             getPages: AppPages.routes,
+//             // home: snapshot.data != null ? HomeView() : LoginView(),
+//           );
+//         }
+//         return LoadingView();
+//       },
+//     );
+//   }
+// }
