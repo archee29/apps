ad0605b6b839a342b234e06c9fd4c1a22daef004
archee29 <mrarchee01@gmail.com>
2023-09-09 23:07:46 +0700

    fixing reset view and controller

--- tugas_akhir/lib/app/modules/reset/views/reset_view.dart
+++ tugas_akhir/lib/app/modules/reset/views/reset_view.dart
@@ -1,156 +1,131 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:tugas_akhir/app/controllers/auth_controller.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:tugas_akhir/app/styles/app_colors.dart';
-import 'package:tugas_akhir/app/widgets/CustomWidgets/custom_button.dart';
-import 'package:tugas_akhir/app/widgets/CustomWidgets/custom_formfield.dart';
-import 'package:tugas_akhir/app/widgets/CustomWidgets/custom_header.dart';
-import 'package:tugas_akhir/app/widgets/CustomWidgets/custom_richtext.dart';
-import 'package:tugas_akhir/app/routes/app_pages.dart';
 
 import '../controllers/reset_controller.dart';
 
 class ResetView extends GetView<ResetController> {
-  final authC = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              color: const Color.fromARGB(255, 239, 62, 255).withOpacity(0.7),
-            ),
-            CustomHeader(
-              text: 'Automatic Cat Feeder',
-              onTap: () {
-                // Navigator.pushReplacement(context,
-                //     MaterialPageRoute(builder: (context) => const SignUp()));
-              },
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.08,
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.9,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                    color: AppColors.whiteshade,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 200,
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      margin: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width * 0.09),
-                      child: Image.asset("assets/images/icon-kucing.png"),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    CustomFormField(
-                      headingText: "Email",
-                      hintText: "Masukkan Email",
-                      icon: const Icon(Icons.email),
-                      obsecureText: false,
-                      suffixIcon: const SizedBox(),
-                      controller: controller.emailController,
-                      maxLines: 1,
-                      textInputAction: TextInputAction.done,
-                      textInputType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    GestureDetector(
-                      child: AuthButton(
-                        onTap: () => authC
-                            .resetPassword(controller.emailController.text),
-                        text: 'Reset Password',
-                      ),
-                    ),
-                    CustomRichText(
-                      discription: "Sudah Punya Akun? ",
-                      text: "Login Disini",
-                      onTap: () => Get.toNamed(Routes.LOGIN),
-                    ),
-                  ],
-                ),
+      backgroundColor: AppColors.primary,
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/arrow-left.svg',
+            color: Colors.white,
+          ),
+          onPressed: () => Get.back(),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 35 / 100,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(left: 32),
+            decoration: BoxDecoration(
+              gradient: AppColors.primaryGradient,
+              image: DecorationImage(
+                image: AssetImage('assets/images/pattern-1-1.png'),
+                fit: BoxFit.cover,
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(0, -2),
-                        blurRadius: 2,
-                      ),
-                    ],
+                Text(
+                  "Automatic Cat Feeder\nMobile Application",
+                  style: TextStyle(
+                    fontSize: 28,
                     color: Colors.white,
+                    fontFamily: 'poppins',
+                    height: 150 / 100,
+                    fontWeight: FontWeight.w600,
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Container(
-                          padding: const EdgeInsets.only(
-                            bottom: 5,
-                            left: 5,
-                            right: 5,
-                          ),
-                          width: 65,
-                          height: 65,
-                          decoration:
-                              BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              offset: const Offset(0, 2),
-                              blurRadius: 2,
-                            ),
-                          ]),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 239, 62, 255)
-                                  .withOpacity(0.7),
-                              shape: BoxShape.circle,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.3),
-                                  offset: const Offset(0, 2),
-                                  blurRadius: 2,
-                                ),
-                              ],
-                            ),
-                            child: const Icon(
-                              Icons.home,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "by Muhammad Aswin Sigit",
+                  style: TextStyle(color: Colors.white),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 65 / 100,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            padding: EdgeInsets.only(left: 20, right: 20, top: 36, bottom: 84),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Reset Password',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Kami Akan Mengirimkan Link Untuk Reset Password\nKe Email Anda.",
+                        style: TextStyle(
+                          color: AppColors.secondarySoft,
+                          height: 150 / 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Obx(
+                  () => Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (controller.isLoading.isFalse) {
+                          await controller.sendEmail();
+                        }
+                      },
+                      child: Text(
+                        (controller.isLoading.isFalse)
+                            ? 'Sedang di Kirim ke Email'
+                            : 'Loading....',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 18),
+                        elevation: 0,
+                        primary: AppColors.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
