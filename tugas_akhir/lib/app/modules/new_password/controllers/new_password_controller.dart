1547f913dfd6a43793a7b17cf6faa90b08fe7331
archee29 <mrarchee01@gmail.com>
2023-09-10 21:55:32 +0700

    fixing file new password view and controller

--- tugas_akhir/lib/app/modules/new_password/controllers/new_password_controller.dart
+++ tugas_akhir/lib/app/modules/new_password/controllers/new_password_controller.dart
@@ -1,23 +1,55 @@
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tugas_akhir/app/widgets/dialog/custom_notification.dart';
 
 class NewPasswordController extends GetxController {
-  //TODO: Implement NewPasswordController
+  RxBool isLoading = false.obs;
+  TextEditingController currentPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  RxBool oldPasswordObs = true.obs;
+  RxBool newPasswordObs = true.obs;
+  RxBool newPasswordControllerObs = true.obs;
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<void> updatePassword() async {
+    if (currentPasswordController.text.isNotEmpty &&
+        newPasswordController.text.isNotEmpty &&
+        confirmNewPasswordController.text.isNotEmpty) {
+      if (newPasswordController.text == confirmNewPasswordController.text) {
+        isLoading.value = true;
+        try {
+          String emailUser = auth.currentUser!.email!;
+          await auth.signInWithEmailAndPassword(
+              email: emailUser, password: currentPasswordController.text);
+          await auth.currentUser!.updatePassword(newPasswordController.text);
+          Get.back();
+          CustomNotification.successNotification(
+              "Sukses", "Berhasil Ubah Password");
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'wrong-password') {
+            CustomNotification.errorNotification(
+                "Error", 'Password Lama Salah');
+          } else {
+            CustomNotification.errorNotification(
+                "error", "Tidak Dapat Mengubah Password Karena : ${e.code}");
+          }
+        } catch (e) {
+          CustomNotification.errorNotification(
+              "Error", "Error : ${e.toString()}");
+        } finally {
+          isLoading.value = false;
+        }
+      } else {
+        CustomNotification.errorNotification("Error",
+            "Password Baru dan Konfirmasi Password yang Dimasukkan Tidak Sama");
+      }
+    } else {
+      CustomNotification.errorNotification(
+          "Error", "Isi Formulir Terlebih Dahulu");
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
