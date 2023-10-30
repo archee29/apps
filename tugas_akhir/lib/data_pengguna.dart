2f6f369c894a83a90d72ffdb6817ae5bff218d69
archee29 <mrarchee01@gmail.com>
2023-10-31 02:04:06 +0700

    fixing git error and add schedule error

--- tugas_akhir/lib/data_pengguna.dart
+++
@@ -1,9 +0,0 @@
-class DataPengguna {
-  static String defaultPassword = 'qwertyuiop';
-  static String defaultRole = 'user';
-
-  static Map<String, dynamic> house = {
-    'latitude': -0.0676835040833068,
-    'longtitude': 109.34238140629837,
-  };
-}
