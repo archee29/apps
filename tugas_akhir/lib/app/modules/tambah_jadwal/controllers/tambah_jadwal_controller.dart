9cae3292d3df7589a5ca4ba91a1242f61fa15538
archee29 <mrarchee01@gmail.com>
2023-11-03 10:54:59 +0700

    fixing tambah jadwal controller

--- tugas_akhir/lib/app/modules/tambah_jadwal/controllers/tambah_jadwal_controller.dart
+++ tugas_akhir/lib/app/modules/tambah_jadwal/controllers/tambah_jadwal_controller.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:tugas_akhir/app/widgets/dialog/custom_alert_dialog.dart';
 import 'package:tugas_akhir/app/widgets/dialog/custom_notification.dart';
 import 'package:tugas_akhir/data_pengguna.dart';
@@ -23,7 +24,6 @@ class TambahJadwalController extends GetxController {
   TextEditingController makananController = TextEditingController();
   TextEditingController minumanController = TextEditingController();
   TextEditingController adminPasswordController = TextEditingController();
-  late final DateTime selectedDate;
 
   RxBool isLoading = false.obs;
   RxBool isLoadingCreateSchedule = false.obs;
@@ -35,36 +35,36 @@ class TambahJadwalController extends GetxController {
     return DataPengguna.defaultPassword;
   }
 
-  Future<void> addSchedule() async {
-    if (idSchedule.text.isNotEmpty &&
-        titleController.text.isNotEmpty &&
-        deskripsiController.text.isNotEmpty &&
-        makananController.text.isNotEmpty &&
-        minumanController.text.isNotEmpty) {
-      isLoading.value = true;
-      CustomAlertDialog.confirmAdmin(
-        title: "Konfirmasi Admin",
-        message:
-            "Konfirmasi Terlebih Dahulu \n Untuk Melakukan Pengisian Tempat Makan dan Minum Kucing",
-        onConfirm: () async {
-          if (isLoadingCreateSchedule.isFalse) {
-            await createSchedule();
-            isLoading.value = false;
-          }
-        },
-        controller: adminPasswordController,
-        onCancel: () {
-          isLoading.value = false;
-          Get.back();
-        },
-      );
-    } else {
-      isLoading.value = false;
-      CustomNotification.errorNotification(
-          "Error", "Isi Form Tambah Jadwal Terlebih Dahul");
-    }
-  }
-
+  // Future<void> addSchedule() async {
+  //   if (idSchedule.text.isNotEmpty &&
+  //       titleController.text.isNotEmpty &&
+  //       deskripsiController.text.isNotEmpty &&
+  //       makananController.text.isNotEmpty &&
+  //       minumanController.text.isNotEmpty) {
+  //     isLoading.value = true;
+  //     CustomAlertDialog.confirmAdmin(
+  //       title: "Konfirmasi Admin",
+  //       message:
+  //           "Konfirmasi Terlebih Dahulu \n Untuk Melakukan Pengisian Tempat Makan dan Minum Kucing",
+  //       onConfirm: () async {
+  //         if (isLoadingCreateSchedule.isFalse) {
+  //           await createSchedule();
+  //           isLoading.value = false;
+  //         }
+  //       },
+  //       controller: adminPasswordController,
+  //       onCancel: () {
+  //         isLoading.value = false;
+  //         Get.back();
+  //       },
+  //     );
+  //   } else {
+  //     isLoading.value = false;
+  //     CustomNotification.errorNotification(
+  //         "Error", "Isi Form Tambah Jadwal Terlebih Dahul");
+  //   }
+  // }
+/*
   createSchedule() async {
     if (adminPasswordController.text.isNotEmpty) {
       isLoadingCreateSchedule.value = true;
@@ -101,6 +101,77 @@ class TambahJadwalController extends GetxController {
     } else {
       CustomNotification.errorNotification(
           "Error", "Masukkan Password Terlebih Dahulu");
+    }
+  }
+  */
+
+  schedule() async {
+    isLoading.value = true;
+    await processSchedule();
+  }
+
+  createSchedule(int docId) async {
+    DocumentReference<Map<String, dynamic>> schedule =
+        firestore.collection("schedule").doc(docId.toString());
+    await schedule.set({
+      "schedule_id": idSchedule,
+      "title": titleController.text,
+      "deskripsi": deskripsiController.text,
+      "makanan": makananController.text,
+      "minuman": minumanController.text,
+      "created_at": DateTime.now().toIso8601String(),
+    });
+  }
+
+  Future<void> addSchedule() async {
+    if (titleController.text.isNotEmpty &&
+        deskripsiController.text.isNotEmpty &&
+        makananController.text.isNotEmpty &&
+        minumanController.text.isNotEmpty) {
+      isLoading.value = true;
+      CustomAlertDialog.confirmAdmin(
+        title: "Konfirmasi Admin",
+        message:
+            "Konfirmasi Terlebih Dahulu \n Untuk Melakukan Pengisian Tempat Makan dan Minum Kucing",
+        onConfirm: () async {
+          if (isLoadingCreateSchedule.isFalse) {
+            await processSchedule();
+            isLoading.value = false;
+          }
+        },
+        controller: adminPasswordController,
+        onCancel: () {
+          isLoading.value = false;
+          Get.back();
+        },
+      );
+    } else {
+      isLoading.value = false;
+      CustomNotification.errorNotification(
+          "Error", "Isi Form Tambah Jadwal Terlebih Dahul");
+    }
+  }
+
+  Future<void> processSchedule() async {
+    String uid = auth.currentUser!.uid;
+    String scheduleDocId =
+        DateFormat.yMd().format(DateTime.now()).replaceAll("/", "-");
+    CollectionReference<Map<String, dynamic>> scheduleCollection =
+        firestore.collection("user").doc(uid).collection("schedule");
+    QuerySnapshot<Map<String, dynamic>> snapshotPreference =
+        await scheduleCollection.get();
+    if (snapshotPreference.docs.isEmpty) {
+      addSchedule();
+    } else {
+      DocumentSnapshot<Map<String, dynamic>> scheduleDoc =
+          await scheduleCollection.doc(scheduleDocId).get();
+      if (scheduleDoc.exists == true) {
+        Map<String, dynamic>? dateScheduleToday = scheduleDoc.data();
+        if (dateScheduleToday?["schedule"] != null) {
+          CustomNotification.successNotification(
+              "Sukses", "Berhasil Menambahkan Jadwal");
+        }
+      }
     }
   }
 }
